203715fd7da3dc670279277e595f200bb96f1569
Nat Keane <natalia.keane@mymona.uwi.edu>
2019-03-17 23:54:32 +0000

    heroku

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "change this to be a more random key"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:password@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://hyioekpiaplpxj:b42b34dee0e7d13a5e3ad113ab6300d472683a9b2bafa8f68cb9109587badbf7@ec2-50-19-109-120.compute-1.amazonaws.com:5432/d8go17643lcfna"# "postgresql://project1:password@localhost/project1" #
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True # added just to suppress a warning
 
 db = SQLAlchemy(app)
